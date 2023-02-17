4fa4ffcf51feb0fe437348034218156a04e252db
DmytroRudenko11 <dimon11rudenko11@gmail.com>
2023-02-17 20:57:21 +0200

    Code upd
    optimized code,
    Added checks on mediarequest,  styles to pagination added.

--- src/js/render-gallery.js
+++ src/js/render-gallery.js
@@ -4,81 +4,85 @@ const box = document.querySelector('.cocktail__list');
 const paginator = document.querySelector('.paginator');
 const paginatorList = document.querySelector('.pagination__list');
 
+let currentPage = 1;
+let perPage = 0;
+let randomArray = [];
+
 async function fetchRandomCoctails() {
-  let randomArray = [];
-  for (let i = 0; i < 9; i++) {
+  perPage = pagesMediaCheck();
+
+  for (let i = 0; i < perPage; i++) {
     const randomCoctail = await axios.get(
       'https://www.thecocktaildb.com/api/json/v1/1/random.php'
     );
 
     randomArray.push(...randomCoctail.data.drinks);
   }
-  console.log(randomArray);
-  return randomArray;
+  return await randomArray;
 }
-async function fetchCocktail() {
+console.log(randomArray);
+
+async function renderRandomCocktails() {
+  const randomList = await fetchRandomCoctails();
+  box.innerHTML = '';
+  buildMarkup(randomList);
+}
+renderRandomCocktails();
+
+async function fetchCocktail(queryToFetch) {
+  if (queryToFetch.lentgh === 1) {
+    const result = await axios.get(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${queryToFetch}`
+    );
+    const cocktails = await result.data.drinks;
+    return cocktails;
+  }
   const result = await axios.get(
-    'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${queryToFetch}`
   );
   const cocktails = await result.data.drinks;
   return cocktails;
 }
 
-async function main() {
-  const cocktails = await fetchRandomCoctails();
+// async function buildGallery() {
+//   const cocktails = await fetchCocktail('a');
+//   perPage = pagesMediaCheck();
 
-  let currentPage = 1;
-  let perPage = pagesMediaCheck();
+//   renderCocktails(cocktails, perPage, currentPage);
 
-  function renderCocktails(cocktailList, perPage, page) {
-    page--;
+//   function displayPagination(cocktailList, perPage) {
+//     const totalPages = Math.ceil(cocktailList.length / perPage);
+//     for (let i = 0; i < totalPages; i++) {
+//       const cocktailItem = displayPaginationBtn(i + 1);
+//       paginatorList.append(cocktailItem);
+//     }
+//   }
 
-    box.innerHTML = '';
-    const begin = perPage * page;
-    const end = begin + perPage;
-    const paginatedData = cocktailList.slice(begin, end);
-    const render = paginatedData.map(({ idDrink, strDrink, strDrinkThumb }) => {
-      const markup = `<li class='cocktail__item'>
-      <div class='cocktail__card'>
-      <img class='cocktail__image' src='${strDrinkThumb}' alt='cocktail'/>
-      <div class='cocktail__thumb'>
-      <h3 class='cocktail__title'>${strDrink}</h3>
-      <div class='cocktail__btn--box'>
-      <button class='learnmore__btn' id='${idDrink}' type='button'>Learn More</button>
-      <button class='add__btn' type='button'>Add to<svg class="icon-hert" width="19" height="17">
-            <use href="/symbol-defs.a8b2e413.svg#icon-heart-transparent"></use>
-          </svg></button>
-      </div>
-      </div>
-      </div>
-      </li>`;
-      box.insertAdjacentHTML('beforeend', markup);
-    });
-  }
+//   function displayPaginationBtn(number) {
+//     const cocktailItem = document.createElement('li');
+//     cocktailItem.classList.add('pagination__item');
+//     cocktailItem.textContent = number;
 
-  function displayPagination(cocktailList, perPage) {
-    const totalPages = Math.ceil(cocktailList.length / perPage);
-    for (let i = 0; i < totalPages; i++) {
-      const cocktailItem = displayPaginationBtn(i + 1);
-      paginatorList.append(cocktailItem);
-    }
-  }
+//     if (currentPage === number) {
+//       cocktailItem.classList.add('pagination__item--active');
+//     }
 
-  function displayPaginationBtn(number) {
-    const cocktailItem = document.createElement('li');
-    cocktailItem.classList.add('pagination__item');
-    cocktailItem.textContent = number;
-    cocktailItem.addEventListener('click', () => {
-      currentPage = number;
-      renderCocktails(cocktails, perPage, currentPage);
-    });
-    return cocktailItem;
-  }
-  renderCocktails(cocktails, perPage, currentPage);
-  displayPagination(cocktails, perPage);
-}
+//     cocktailItem.addEventListener('click', () => {
+//       currentPage = number;
+//       renderCocktails(cocktails, perPage, currentPage);
 
-main();
+//       let currentActive = document.querySelector('li.pagination__item--active');
+//       currentActive.classList.remove('pagination__item--active');
+
+//       cocktailItem.classList.add('pagination__item--active');
+//     });
+//     return cocktailItem;
+//   }
+
+//   displayPagination(cocktails, perPage);
+// }
+
+// buildGallery();
 
 function pagesMediaCheck() {
   if (window.matchMedia('(min-width: 320px)').matches) {
@@ -92,4 +96,36 @@ function pagesMediaCheck() {
   }
 
   return perPage;
+}
+
+function buildMarkup(data) {
+  const markup = data.map(({ strDrinkThumb, strDrink, idDrink }) => {
+    return `<li class='cocktail__item'>
+      <div class='cocktail__card'>
+      <img class='cocktail__image' src='${strDrinkThumb}' alt='cocktail'/>
+      <div class='cocktail__thumb'>
+      <h3 class='cocktail__title'>${strDrink}</h3>
+      <div class='cocktail__btn--box'>
+      <button class='learnmore__btn' type='button'>Learn More</button>
+      <button class='add__btn' data-id='${idDrink}' type='button'>Add to
+     <svg class="icon-hert" width="19" height="17">
+            <use href="./symbol-defs.a8b2e413.svg#icon-heart-transparent"></use>
+    </svg>
+     </button>
+      </div>
+      </div>
+      </div>
+      </li>`;
+  });
+  box.insertAdjacentHTML('beforeend', markup.join(''));
+}
+
+function renderCocktails(cocktailList, perPage, page) {
+  page--;
+  box.innerHTML = '';
+
+  const begin = perPage * page;
+  const end = begin + perPage;
+  const paginatedData = cocktailList.slice(begin, end);
+  buildMarkup(paginatedData);
 }
