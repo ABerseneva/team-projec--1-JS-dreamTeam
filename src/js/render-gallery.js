46f0c3e22c7bf6f7d925a971653ef7adb88dcea1
DmytroRudenko11 <dimon11rudenko11@gmail.com>
2023-02-17 14:41:00 +0200

    Upd

--- src/js/render-gallery.js
+++ src/js/render-gallery.js
@@ -4,6 +4,18 @@ const box = document.querySelector('.cocktail__list');
 const paginator = document.querySelector('.paginator');
 const paginatorList = document.querySelector('.pagination__list');
 
+async function fetchRandomCoctails() {
+  let randomArray = [];
+  for (let i = 0; i < 9; i++) {
+    const randomCoctail = await axios.get(
+      'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+    );
+
+    randomArray.push(...randomCoctail.data.drinks);
+  }
+  console.log(randomArray);
+  return randomArray;
+}
 async function fetchCocktail() {
   const result = await axios.get(
     'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
@@ -13,10 +25,10 @@ async function fetchCocktail() {
 }
 
 async function main() {
-  const cocktails = await fetchCocktail();
+  const cocktails = await fetchRandomCoctails();
 
   let currentPage = 1;
-  let perPage = 3;
+  let perPage = pagesMediaCheck();
 
   function renderCocktails(cocktailList, perPage, page) {
     page--;
@@ -25,17 +37,16 @@ async function main() {
     const begin = perPage * page;
     const end = begin + perPage;
     const paginatedData = cocktailList.slice(begin, end);
-
-    const render = paginatedData.map(({ strDrink, strDrinkThumb }) => {
+    const render = paginatedData.map(({ idDrink, strDrink, strDrinkThumb }) => {
       const markup = `<li class='cocktail__item'>
       <div class='cocktail__card'>
       <img class='cocktail__image' src='${strDrinkThumb}' alt='cocktail'/>
       <div class='cocktail__thumb'>
       <h3 class='cocktail__title'>${strDrink}</h3>
       <div class='cocktail__btn--box'>
-      <button class='learnmore__btn' type='button'>Learn More</button>
+      <button class='learnmore__btn' id='${idDrink}' type='button'>Learn More</button>
       <button class='add__btn' type='button'>Add to<svg class="icon-hert" width="19" height="17">
-            <use href="./images/symbol-defs.svg#icon-heart-transparent"></use>
+            <use href="/symbol-defs.a8b2e413.svg#icon-heart-transparent"></use>
           </svg></button>
       </div>
       </div>
@@ -68,3 +79,17 @@ async function main() {
 }
 
 main();
+
+function pagesMediaCheck() {
+  if (window.matchMedia('(min-width: 320px)').matches) {
+    perPage = 3;
+  }
+  if (window.matchMedia('(min-width: 768px)').matches) {
+    perPage = 6;
+  }
+  if (window.matchMedia('(min-width: 1200px)').matches) {
+    perPage = 9;
+  }
+
+  return perPage;
+}
