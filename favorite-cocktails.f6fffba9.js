0bb8cd929d9927f003105569d7a55341991ad25c
ABerseneva <117768862+ABerseneva@users.noreply.github.com>
2023-02-18 11:06:35 +0000

    Deploying to gh-pages from @ ABerseneva/team-projec--1-JS-dreamTeam@dded8805ab8fc3384c70aa957fc54afc81572b47 🚀

---
+++ favorite-cocktails.f6fffba9.js
@@ -0,0 +1,2 @@
+document.querySelector('.theme-switch input[type="checkbox"]').addEventListener("change",(function(e){e.target.checked?(document.documentElement.setAttribute("data-theme","dark"),localStorage.setItem("theme","dark")):(document.documentElement.setAttribute("data-theme","light"),localStorage.setItem("theme","light"))}),!1);
+//# sourceMappingURL=favorite-cocktails.f6fffba9.js.map
