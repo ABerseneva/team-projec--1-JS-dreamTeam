649bdde0b30839825127cb848931dcb89b8c42bd
makoviisergii <sergiimakovii78@gmail.com>
2023-02-19 22:29:58 +0200

    Revert "Merge branch 'mobal-cocktails' of https://github.com/makoviisergii/team-projec--1-JS-dreamTeam into mobal-cocktails"
    This reverts commit eb5a11bd68257ab45cd780a91896b110dadaed07, reversing
    changes made to df7382b125e83ae2313b2f44925c22dc6c57ad2c.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import './js/coctail-card';
 import './js/favorite-list';
 import './js/hero-alphabet';
-// import './js/local-storage';
-import './js/add-from-gallery';
+import './js/local-storage';
 import './js/mobile-menu';
 import './js/modal-coctails';
 import './js/modal-ingridients';
 import './js/pagination';
 import './js/render-gallery';
-import './js/color-switch';
-import './js/fetch';
-import './js/local-storage-ingredients';
