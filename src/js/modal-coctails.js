df7382b125e83ae2313b2f44925c22dc6c57ad2c
makoviisergii <sergiimakovii78@gmail.com>
2023-02-19 22:27:07 +0200

    19.02 22.20

--- src/js/modal-coctails.js
+++ src/js/modal-coctails.js
@@ -27,7 +27,7 @@ import axios from 'axios';
 const favoriteCard = document.querySelector('.favorite-card');
 const name = document.querySelector('.modal-cockt__name');
 const instractions = document.querySelector('.modal-cockt__instr-text');
-const perList = document.querySelector('.modal-cockt__per-list');
+const list = document.querySelector('.modal-cockt__per-list');
 const image = document.querySelector('.modal-cockt__img');
 
 favoriteCard.addEventListener('click', e => {
@@ -59,4 +59,41 @@ function renderCocktailCard(cocktail) {
   image.src = `${cocktail.strDrinkThumb}`;
   name.textContent = `${cocktail.strDrink}`;
   instractions.textContent = `${cocktail.strInstructions}`;
+  let perList = [];
+  list.innerHTML = '';
+  for (let i = 1; i < 15; i++) {
+    if (cocktail[`strMeasure${i}`] || cocktail[`strIngredient${i}`]) {
+      perList.push(
+        `<li class="modal-cockt__ingred">
+          <p>${cocktail[`strMeasure${i}`]}
+          <span data-ingredient="${cocktail[`strIngredient${i}`]}">
+            ${cocktail[`strIngredient${i}`]}
+          </span></p>
+        </li>`
+      );
+    }
+  }
+  list.insertAdjacentHTML('beforeend', perList.join(''));
+
+  function markupingBtn() {
+    const btns = document.querySelectorAll('[data-add]');
+    console.log(btns);
+    btns.forEach(btn => {
+      const card = btn.closest('.item');
+      const coctId = card.dataset.id;
+      const coctailList = JSON.parse(localStorage.getItem('coctailse'));
+      const numbers = coctailList.map(({ id }) => id);
+      if (numbers.some(value => value === coctId)) {
+        const tekst = btn.textContent === 'Add to favorite';
+        if (tekst) {
+          btn.textContent = 'Remove from favorite';
+        } else {
+          btn.innerHTML = `Remove
+     <svg class="icon-hert" width="19" height="17">
+            <use href="./symbol-defs.a8b2e413.svg#icon-heart-transparent"></use>
+    </svg>`;
+        }
+      }
+    });
+  }
 }
