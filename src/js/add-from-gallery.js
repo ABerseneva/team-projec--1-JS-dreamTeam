0c03dfa91088fda084bc4159b0510a13c503f658
VickyBondarenko <quikyv@gmail.com>
2023-02-20 16:47:14 +0100

    Add search for favorit

--- src/js/add-from-gallery.js
+++ src/js/add-from-gallery.js
@@ -1,24 +1,22 @@
+// import personalheart from '../images/personalheart.svg';
+
 const addBtn = document.querySelectorAll('.add__btn');
 const coctailse = [];
-// const btn = document.querySelectorAll('[data-add]');
-// console.log(btn);
+
 if (!localStorage.getItem('coctailse')) {
   localStorage.setItem('coctailse', JSON.stringify(coctailse));
 }
 
 const coctailList = JSON.parse(localStorage.getItem('coctailse'));
 
-console.log(coctailList);
-
 document.addEventListener('click', event => {
   if (event.target.dataset.add != undefined) {
     const coctailList = JSON.parse(localStorage.getItem('coctailse'));
     const card = event.target.closest('.item');
-    // console.log(card);
     const coctId = card.dataset.id;
 
     const numbers = coctailList.map(({ id }) => id);
-    // console.log(coctailList.map(({ id }) => id));
+
     if (
       numbers.every(value => value !== coctId) ||
       localStorage.getItem('coctailse') === '[]'
@@ -32,13 +30,12 @@ document.addEventListener('click', event => {
         src: coctImg,
       };
       coctailList.push(favoritCoct);
-      // console.log(coctailList);
       localStorage.setItem('coctailse', JSON.stringify(coctailList));
 
       markupingBtn();
     } else if (numbers.some(value => value === coctId)) {
       const filteredCoctails = coctailList.filter(({ id }) => id !== coctId);
-      console.log(filteredCoctails);
+
       localStorage.setItem('coctailse', JSON.stringify(filteredCoctails));
       changeToAdd(event);
     }
@@ -51,28 +48,27 @@ function changeToAdd(event) {
     event.target.textContent = 'Add to favorite';
   } else {
     event.target.innerHTML = `Add to
-     <svg class="icon-hert" width="19" height="17">
-            <use href="./symbol-defs.a8b2e413.svg#icon-heart-transparent"></use>
-    </svg>`;
+   <svg class="icon-hert" width="17" height="15">
+     <use href="${personalheart + '#icon-black'}"></use>
+   </svg>`;
   }
 }
 
-function changeToRemove(event) {
-  const tekst = event.target.textContent === 'Add to favorite';
+// function changeToRemove(event) {
+//   const tekst = event.target.textContent === 'Add to favorite';
 
-  if (tekst) {
-    event.target.textContent = 'Remove from favorite';
-  } else {
-    event.target.innerHTML = `Remove
-     <svg class="icon-hert" width="19" height="17">
-            <use href="./symbol-defs.a8b2e413.svg#icon-heart-transparent"></use>
-    </svg>`;
-  }
-}
+//   if (tekst) {
+//     event.target.textContent = 'Remove from favorite';
+//   } else {
+//     event.target.innerHTML = `Remove
+//      <svg class="icon-hert" width="19" height="17">
+//             <use href="./symbol-defs.a8b2e413.svg#icon-heart-transparent"></use>
+//     </svg>`;
+//   }
+// }
 
 export function markupingBtn() {
   const btns = document.querySelectorAll('[data-add]');
-  console.log(btns);
   btns.forEach(btn => {
     const card = btn.closest('.item');
     const coctId = card.dataset.id;
@@ -85,9 +81,9 @@ export function markupingBtn() {
         btn.textContent = 'Remove from favorite';
       } else {
         btn.innerHTML = `Remove
-     <svg class="icon-hert-fill" width="19" height="17">
-            <use href="./symbol-defs.a8b2e413.svg#icon-heart-orange"></use>
-    </svg>`;
+    <svg class="icon-hert" width="17" height="15">
+     <use href="${personalheart + '#icon-black'}"></use>
+   </svg>`;
       }
     }
   });
--- src/js/render-favor-coctail.js
+++ src/js/render-favor-coctail.js
@@ -1,8 +1,11 @@
+// import personalheart from '../images/personalheart.svg';
+
 const box = document.querySelector('.cocktail__list');
 const paginator = document.querySelector('.paginator');
 const paginatorList = document.querySelector('.pagination__list');
 const sectionGallery = document.querySelector('.cocktail__section');
 const wrapper = document.querySelector('.favorit-coct__wrapper');
+const coctailse = [];
 
 let currentPage = 1;
 let perPage = 0;
@@ -22,14 +25,10 @@ function pagesMediaCheck() {
   return perPage;
 }
 
-const coctailse = [];
-
 if (!localStorage.getItem('coctailse')) {
   localStorage.setItem('coctailse', JSON.stringify(coctailse));
 }
-
 const coctailList = JSON.parse(localStorage.getItem('coctailse'));
-// console.log(coctailList);
 
 if (localStorage.getItem('coctailse') !== '[]') {
   wrapper.innerHTML = '';
@@ -38,9 +37,34 @@ if (localStorage.getItem('coctailse') !== '[]') {
   buildMarkup(coctailList);
 }
 
-function buildMarkup(data) {
+document.addEventListener('click', event => {
+  if (event.target.dataset.add != undefined) {
+    const coctailList = JSON.parse(localStorage.getItem('coctailse'));
+    const card = event.target.closest('.item');
+    const coctId = card.dataset.id;
+    const numbers = coctailList.map(({ id }) => id);
+
+    if (numbers.some(value => value === coctId)) {
+      const filteredCoctails = coctailList.filter(({ id }) => id !== coctId);
+      localStorage.setItem('coctailse', JSON.stringify(filteredCoctails));
+      box.innerHTML = '';
+      buildMarkup(filteredCoctails);
+
+      if (localStorage.getItem('coctailse') === '[]') {
+        renderStartMarkup();
+      }
+    }
+  }
+});
+
+function renderStartMarkup() {
+  wrapper.style.display = 'block';
+  const startMarkup = `<p class="favorit-coct__text">You haven't added any favorite cocktails yet</p>`;
+  wrapper.insertAdjacentHTML('beforeend', startMarkup);
+}
+
+export function buildMarkup(data) {
   const markup = data.map(({ id, name, src }) => {
-    // console.log(src);
     return `   <li class='cocktail__item item' data-id='${id}''>
       <img class='cocktail__image image' src="${src}" alt='cocktail'/>
       <h3 class='cocktail__title name'>${name}</h3>
@@ -56,40 +80,3 @@ function buildMarkup(data) {
   });
   box.insertAdjacentHTML('beforeend', markup.join(''));
 }
-
-// document.addEventListener('click', event => {
-//   if (event.target.dataset.add != undefined) {
-//     const coctailList = JSON.parse(localStorage.getItem('coctailse'));
-//     const card = event.target.closest('.item');
-//     console.log(card);
-//     const coctId = card.dataset.id;
-
-//     const numbers = coctailList.map(({ id }) => id);
-//     console.log(coctailList.map(({ id }) => id));
-//     if (
-//       numbers.every(value => value !== coctId) ||
-//       localStorage.getItem('coctailse') === '[]'
-//     ) {
-//       const coctImg = card.querySelector('.image').src;
-//       const coctName = card.querySelector('.name').textContent;
-
-//       const favoritCoct = {
-//         id: coctId,
-//         name: coctName,
-//         src: coctImg,
-//       };
-//       coctailList.push(favoritCoct);
-//       console.log(coctailList);
-//       localStorage.setItem('coctailse', JSON.stringify(coctailList));
-
-//       changeToRemove(event);
-//     } else if (numbers.some(value => value === coctId)) {
-//       const filteredCoctails = coctailList.filter(({ id }) => id !== coctId);
-//       console.log(filteredCoctails);
-//       localStorage.setItem('coctailse', JSON.stringify(filteredCoctails));
-//       changeToAdd(event);
-//       box.innerHTML = '';
-//       buildMarkup(coctailList);
-//     }
-//   }
-// });
