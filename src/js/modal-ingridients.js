9e703bd49ac3e020312a265be98b8fce8d478c6e
dyonns <117724779+dyonns@users.noreply.github.com>
2023-02-20 14:38:52 +0200

    modal-ingredient Edit 1

--- src/js/modal-ingridients.js
+++ src/js/modal-ingridients.js
@@ -1,27 +1,73 @@
-import axios from 'axios';
-
-
-
-
-
-
-
-
-
-
-
-
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-ingrid-open]"),
+    openModalBtns: document.querySelectorAll("[data-modal-ingrid-open]"),
     closeModalBtn: document.querySelector("[data-modal-ingrid-close]"),
     modal: document.querySelector("[data-modal-ingrid]"),
+    body: document.querySelector('body'),
+    // noBackdrop: document.querySelector("[data-modal-ingrid-noBackdrop]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtns.forEach((btn) => {
+    btn.addEventListener("click", toggleModal);
+  });
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.modal.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle('no-scroll');
   }
+
+  // if (target !== refs.noBackdrop) {
+  //   refs.modal.classList.add("is-hidden");
+  //   refs.body.classList.remove("no-scroll");
+  // }
+
 })();
+// -------------------------------------------------------------------------------------
+
+import axios from 'axios';
+
+const box = document.querySelector('.box-ingrid');
+const ingridClick = Array.from(document.querySelectorAll('.modal-cockt__ingred'));
+
+ingridClick.forEach((ingrid) => {
+  ingrid.addEventListener('click', async () => {
+    const ingridName = ingrid.dataset.name;
+    const ingridData = await fetchIngridient(ingridName);
+    buildMarkup(ingridData);
+  });
+});
+
+async function fetchIngridient(queryToFetch) {
+  try {
+    const result = await axios.get(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${queryToFetch}`
+    );
+    return result.data.ingredients[0];
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function buildMarkup(data) {
+    const {
+    strIngredient: name,
+    strType: type,
+    strDescription: description,
+  } = data;
+    const markup = `<h2 class="modal-ingrid__name">${name}</h2>
+                <h3 class="modal-ingrid__title">${name}</h3>
+                <div class="line"></div>
+    
+                <p class="modal-ingrid__descrip">${description}</p>
+                <ul class="modal-ingrid__list">
+                    <li class="modal-ingrid__component"><p><span>&#10038;</span> Type: ${type}</p></li>
+                    <li class="modal-ingrid__component"><p><span>&#10038;</span> Country of origin: Italy</p> </li>
+                    <li class="modal-ingrid__component"><p><span>&#10038;</span> Alcohol by volume: 20.5–28.5%</p> </li>
+                    <li class="modal-ingrid__component"><p><span>&#10038;</span> Flavour: Bitter, spicy and sweet</p></li>
+                </ul>`;
+    box.innerHTML = markup;
+}
+
+
