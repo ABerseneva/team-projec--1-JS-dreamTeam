1da324336f429ff433b093ab35c1052035967585
makoviisergii <sergiimakovii78@gmail.com>
2023-02-20 14:24:48 +0200

    js

--- src/js/modal-coctails.js
+++ src/js/modal-coctails.js
@@ -24,22 +24,23 @@
 
 import axios from 'axios';
 
-const favoriteCard = document.querySelector('.favorite-card');
+const learnMoreBtn = document.querySelector('.learn-more-btn');
 const name = document.querySelector('.modal-cockt__name');
 const instractions = document.querySelector('.modal-cockt__instr-text');
-const perList = document.querySelector('.modal-cockt__per-list');
+const list = document.querySelector('.modal-cockt__per-list');
 const image = document.querySelector('.modal-cockt__img');
 
-favoriteCard.addEventListener('click', e => {
-  const cocktaiId = Number(e.currentTarget.dataset.cocktail);
-  console.log(cocktaiId);
-  getCocktailData(cocktaiId);
+document.addEventListener('click', e => {
+  if (e.target === learnMoreBtn) {
+    const cocktaiId = Number(e.target.dataset.cocktail);
+    getCocktailData(cocktaiId);
+  }
+  return;
 });
 
 async function getCocktailData(cocktaiId) {
   const cocktail = await fetchCocktail(cocktaiId);
 
-  console.log(cocktail);
   renderCocktailCard(cocktail);
 }
 
@@ -59,4 +60,53 @@ function renderCocktailCard(cocktail) {
   image.src = `${cocktail.strDrinkThumb}`;
   name.textContent = `${cocktail.strDrink}`;
   instractions.textContent = `${cocktail.strInstructions}`;
+  let perList = [];
+  list.innerHTML = '';
+  for (let i = 1; i < 15; i++) {
+    if (cocktail[`strMeasure${i}`] || cocktail[`strIngredient${i}`]) {
+      if (!cocktail[`strMeasure${i}`]) {
+        perList.push(
+          `<li class="modal-cockt__ingred">
+          <p>
+          <span data-ingredient="${cocktail[`strIngredient${i}`]}">
+            ${cocktail[`strIngredient${i}`]}
+          </span></p>
+        </li>`
+        );
+        break;
+      }
+      perList.push(
+        `<li class="modal-cockt__ingred">
+          <p>${cocktail[`strMeasure${i}`]}
+          <span data-ingredient="${cocktail[`strIngredient${i}`]}">
+            ${cocktail[`strIngredient${i}`]}
+          </span></p>
+        </li>`
+      );
+    }
+  }
+  list.insertAdjacentHTML('beforeend', perList.join(''));
+  markupingBtn();
+}
+
+function markupingBtn() {
+  const btns = document.querySelectorAll('[data-add]');
+  // console.log(btns);
+  btns.forEach(btn => {
+    const card = btn.closest('.item');
+    const coctId = card.dataset.id;
+    const coctailList = JSON.parse(localStorage.getItem('coctailse'));
+    const numbers = coctailList.map(({ id }) => id);
+    if (numbers.some(value => value === coctId)) {
+      const tekst = btn.textContent === 'Add to favorite';
+      if (tekst) {
+        btn.textContent = 'Remove from favorite';
+      } else {
+        btn.innerHTML = `Remove
+     <svg class="icon-hert" width="19" height="17">
+            <use href="./symbol-defs.a8b2e413.svg#icon-heart-transparent"></use>
+    </svg>`;
+      }
+    }
+  });
 }
